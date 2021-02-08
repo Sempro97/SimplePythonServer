5f57a948edf7882de3989f98ff16b9dba97780cf
Semprini Mattia <mattia.semprini2@studio.unibo.it>
2021-02-08 22:17:41 +0100

    First render of index

--- source/main.py
+++ source/main.py
@@ -1,10 +1,20 @@
 import os
-from flask import Flask, jsonify, request
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, World'
+@app.route('/<article>')
+def hello(article=None):
+    url_for('static', filename='assets/img/favicon.png')
+    url_for('static', filename='assets/img/apple-touch-icon.png')
+    url_for('static', filename='assets/vendor/bootstrap/css/bootstrap.min.css')
+    url_for('static', filename='assets/vendor/icofont/icofont.min.css')
+    url_for('static', filename='assets/vendor/boxicons/css/boxicons.min.css')
+    url_for('static', filename='assets/vendor/venobox/venobox.css')
+    url_for('static', filename='assets/vendor/owl.carousel/assets/owl.carousel.min.css')
+    url_for('static', filename='assets/vendor/aos/aos.css')
+    url_for('static', filename='/assets/css/style.css')
+    return render_template('index.html', article=article)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
