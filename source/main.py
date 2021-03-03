8465ecdd0757a939fe9ae8fb05db7e284ab3f275
Semprini Mattia <mattia.semprini2@studio.unibo.it>
2021-03-03 21:14:06 +0100

    Ended article page using flask templates

--- source/main.py
+++ source/main.py
@@ -1,18 +1,37 @@
 import os
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, send_from_directory, abort
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/<article>')
-def hello(article=None):
-    return render_template('index.html', article=article)
+def index():
+    f = open("/usr/src/app/templates/index.html", "r")
+    file = f.read()
+    return render_template('index.html', title = "Index", css="style.css", template=file)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        return do_the_login()
+@app.route('/<filename>')
+def download_file(filename):
+    return send_from_directory('/usr/src/app/pdf/',
+                               filename, as_attachment=True)
+
+
+@app.route('/article')
+@app.route('/article<article>')
+def hello(article):
+    if int(article) > 0 and int(article) < 5:
+        f = open("/usr/src/app/static/assets/articles/" + article + ".txt", "r")
+        baseFile=open("/usr/src/app/templates/article.html", "r")
+        file = baseFile.read()
+        title = f.readline()
+        article = f.readline()
+        imagePath = f.readline()
+        return render_template('article.html',
+            css="style.css",
+            template=file,
+            title=title, 
+            textArticle=article,
+            imagePath=imagePath)
     else:
-        return show_the_login_form()
+        return index()
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=os.getenv('PORT'))
